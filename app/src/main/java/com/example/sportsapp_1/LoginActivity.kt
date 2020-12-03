5eabb6cd1e22a3b64775a54a99ec30ff32e1611b
Ugur Yıldırım <uguryldrm_limbo@hotmail.com>
2020-12-04 02:17:22 +0300

    deneme commit

--- app/src/main/java/com/example/sportsapp_1/LoginActivity.kt
+++ app/src/main/java/com/example/sportsapp_1/LoginActivity.kt
@@ -5,7 +5,7 @@ import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.login.*
 
-class LoginActivity : AppCompatActivity() {
+class  LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.login)
