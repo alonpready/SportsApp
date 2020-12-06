606d3a96211f97949d8bf8aaab6357b4787ffe05
OnurcanKoroglu <korogluonurcan@gmail.com>
2020-12-06 05:11:27 +0300

    bombooş

--- app/src/main/java/com/example/sportsapp_1/HomepageFragment.kt
+++ app/src/main/java/com/example/sportsapp_1/HomepageFragment.kt
@@ -3,16 +3,14 @@ package com.example.sportsapp_1
 import android.content.Intent
 import android.os.Bundle
 import android.util.Log
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
 import androidx.activity.OnBackPressedCallback
+import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentManager
 import androidx.fragment.app.FragmentTransaction
 import kotlinx.android.synthetic.main.fragment_homepage.*
-import kotlinx.android.synthetic.main.fragment_user.*
 
 
 class HomepageFragment : Fragment() {
