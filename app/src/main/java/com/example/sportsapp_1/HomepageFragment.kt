56b5f6e7e3d8abcc9658cf349caf5d7788a7a529
OnurcanKoroglu <korogluonurcan@gmail.com>
2020-12-06 22:24:25 +0300

    Recycleview Added.

--- app/src/main/java/com/example/sportsapp_1/HomepageFragment.kt
+++ app/src/main/java/com/example/sportsapp_1/HomepageFragment.kt
@@ -1,5 +1,6 @@
 package com.example.sportsapp_1
 
+import android.content.AbstractThreadedSyncAdapter
 import android.content.Intent
 import android.os.Bundle
 import android.util.Log
@@ -11,15 +12,20 @@ import android.widget.TextView
 import androidx.activity.OnBackPressedCallback
 import androidx.fragment.app.FragmentManager
 import androidx.fragment.app.FragmentTransaction
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.fragment_homepage.*
 import kotlinx.android.synthetic.main.fragment_user.*
 
 
 class HomepageFragment : Fragment() {
 
+    private lateinit var listOfTrainingTypes:ArrayList<TrainingTypes>
+    private lateinit var adapter:RVAdapter
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
 
         activity?.onBackPressedDispatcher?.addCallback(this, object : OnBackPressedCallback(true){
             override fun handleOnBackPressed() {
@@ -43,10 +49,31 @@ class HomepageFragment : Fragment() {
 
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
+            super.onViewCreated(view, savedInstanceState)
+
         iv_homepage_photo.setOnClickListener() {
             loadFragment(UserFragment())
         }
+
+        rv.layoutManager = LinearLayoutManager(activity)
+
+        val t1 = TrainingTypes("Dead Lift","3x5")
+        val t2 = TrainingTypes("Bench Press","2x10")
+        val t3 = TrainingTypes("Pull Dumbell","4x2")
+        val t4 = TrainingTypes("Dumbell Lift","2x5")
+        val t5 = TrainingTypes("Barfix","3x5")
+        val t6 = TrainingTypes("Squad","2x6")
+
+        listOfTrainingTypes = ArrayList<TrainingTypes>()
+        listOfTrainingTypes.add(t1)
+        listOfTrainingTypes.add(t2)
+        listOfTrainingTypes.add(t3)
+        listOfTrainingTypes.add(t4)
+        listOfTrainingTypes.add(t5)
+        listOfTrainingTypes.add(t6)
+
+        rv.adapter = RVAdapter(requireContext(),listOfTrainingTypes)
+
     }
 
     private fun loadFragment(fragment: Fragment) {
---
+++ app/src/main/java/com/example/sportsapp_1/RVAdapter.kt
@@ -0,0 +1,59 @@
+package com.example.sportsapp_1
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import android.widget.Toast
+import androidx.cardview.widget.CardView
+import androidx.recyclerview.widget.RecyclerView
+
+class RVAdapter(private val mContext: Context, private val trainingList: List<TrainingTypes>) :
+    RecyclerView.Adapter<RVAdapter.CardViewHolderOfDesignObjects>() {
+
+    inner class CardViewHolderOfDesignObjects(view: View) : RecyclerView.ViewHolder(view) {
+
+        var homepageCardView: CardView
+        var tv_training_type: TextView
+        var tv_training_repetition: TextView
+        var iv_training_playIcon: ImageView
+
+        init {
+            homepageCardView = view.findViewById(R.id.cv_cardview)
+            tv_training_type = view.findViewById(R.id.tv_training_type)
+            tv_training_repetition = view.findViewById(R.id.tv_training_repetition)
+            iv_training_playIcon = view.findViewById(R.id.iv_training_playIcon)
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CardViewHolderOfDesignObjects {
+
+        val view = LayoutInflater.from(mContext).inflate(R.layout.card_design, parent, false)
+        return CardViewHolderOfDesignObjects(view)
+
+    }
+
+    override fun onBindViewHolder(holder: CardViewHolderOfDesignObjects, position: Int) {
+
+        val training = trainingList[position]
+
+        holder.tv_training_type.text = training.trainingName
+
+        holder.tv_training_repetition.text = training.trainingRepetition
+
+        holder.homepageCardView.setOnClickListener {
+            Toast.makeText(
+                mContext, "Bu işlem ${training.trainingName} url'sine gidecek.", Toast
+                    .LENGTH_SHORT
+            ).show()
+
+        }
+    }
+
+    override fun getItemCount(): Int {
+
+        return trainingList.size
+    }
+}
