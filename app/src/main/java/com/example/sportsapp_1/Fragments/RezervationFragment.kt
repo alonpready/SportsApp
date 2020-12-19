f07e67a8bfa23aa156d9db187fb281756e3f430c
Ugur Yıldırım <uguryldrm_limbo@hotmail.com>
2020-12-19 21:53:54 +0300

    no message

--- app/src/main/java/com/example/sportsapp_1/Fragments/RezervationFragment.kt
+++ app/src/main/java/com/example/sportsapp_1/Fragments/RezervationFragment.kt
@@ -1,13 +1,15 @@
 package com.example.sportsapp_1.Fragments
 
+import android.app.DatePickerDialog
+import android.app.TimePickerDialog
 import android.content.Intent
 import android.os.Bundle
 import android.util.Log
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.activity.OnBackPressedCallback
+import androidx.fragment.app.Fragment
 import coil.load
 import com.example.sportsapp_1.Model.UserValues
 import com.example.sportsapp_1.R
@@ -19,6 +21,9 @@ import com.google.firebase.database.DatabaseError
 import com.google.firebase.database.FirebaseDatabase
 import com.google.firebase.database.ValueEventListener
 import kotlinx.android.synthetic.main.fragment_rezervation.*
+import java.text.DateFormat
+import java.text.SimpleDateFormat
+import java.util.*
 
 
 class RezervationFragment : Fragment() {
@@ -26,7 +31,7 @@ class RezervationFragment : Fragment() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        activity?.onBackPressedDispatcher?.addCallback(this, object : OnBackPressedCallback(true){
+        activity?.onBackPressedDispatcher?.addCallback(this, object : OnBackPressedCallback(true) {
             override fun handleOnBackPressed() {
                 Log.d("CDA", "onBackPressed Called")
                 val setIntent = Intent(Intent.ACTION_MAIN)
@@ -52,7 +57,7 @@ class RezervationFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-
+        setClicks()
         rezpage_cl.Gone()
         rezPage_progressbar.Visible()
         userInfoLoad()
@@ -62,8 +67,49 @@ class RezervationFragment : Fragment() {
 
 
 
+
     }
 
+
+    private fun setClicks() {
+        button.setOnClickListener { pickDateTime() }
+    }
+    private fun pickDateTime() {
+        val currentDateTime = Calendar.getInstance()
+        val startYear = currentDateTime.get(Calendar.YEAR)
+        val startMonth = currentDateTime.get(Calendar.MONTH)
+        val startDay = currentDateTime.get(Calendar.DAY_OF_MONTH)
+        val startHour = currentDateTime.get(Calendar.HOUR_OF_DAY)
+        val startMinute = currentDateTime.get(Calendar.MINUTE)
+
+        DatePickerDialog(
+            requireContext(),
+            DatePickerDialog.OnDateSetListener { _, year, month, day ->
+                TimePickerDialog(
+                    requireContext(),
+                    TimePickerDialog.OnTimeSetListener { _, hour, minute ->
+                        val pickedDateTime = Calendar.getInstance()
+                        pickedDateTime.set(year, month, day, hour, minute)
+                        doSomethingWith(pickedDateTime)
+                    },
+                    startHour,
+                    startMinute,
+                    true
+                ).show()
+            },
+            startYear,
+            startMonth,
+            startDay
+        ).show()
+    }
+
+    private fun doSomethingWith(pickedDateTime: Calendar) {
+        val date = pickedDateTime.time
+        val dateFormat: DateFormat = SimpleDateFormat("yyyy-mm-dd hh:mm")
+        val strDate: String = dateFormat.format(date)
+
+        editTextDate.setText(strDate)
+    }
 
 
     private fun loadFragment(fragment: Fragment) {
@@ -94,7 +140,7 @@ class RezervationFragment : Fragment() {
         })
 
     }
-    private fun minippLoad(photoUrl:String?) {
+    private fun minippLoad(photoUrl: String?) {
         if (photoUrl != ""){
             iv_rezervation_profile_photo.load(photoUrl)
         }
