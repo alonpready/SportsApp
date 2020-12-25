ffbc48d5e9ee919cd414df88bd4d9c42b39662d0
OnurcanKoroglu <korogluonurcan@gmail.com>
2020-12-26 00:50:04 +0300

    Çeşitli değişiklikler.

--- app/src/main/java/com/example/sportsapp_1/Fragments/ToolbarMenuPage/SettingsFragment.kt
+++ app/src/main/java/com/example/sportsapp_1/Fragments/ToolbarMenuPage/SettingsFragment.kt
@@ -1,10 +1,12 @@
 package com.example.sportsapp_1.Fragments.ToolbarMenuPage
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Switch
+import android.widget.Toast
+import androidx.fragment.app.Fragment
 import com.example.sportsapp_1.Fragments.UserFragment
 import com.example.sportsapp_1.R
 import kotlinx.android.synthetic.main.inside_fragment_connection.*
@@ -25,14 +27,23 @@ class SettingsFragment : Fragment() {
         return inflater.inflate(R.layout.inside_fragment_settings, container, false)
     }
 
+
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-        iv_Connection_back_button.setOnClickListener{
+        iv_Connection_back_button.setOnClickListener {
             loadFragment(UserFragment())
         }
 
+        val switch = view.findViewById<Switch>(R.id.s_Settings_swtich)
+        switch.setOnCheckedChangeListener { _, isChecked ->
+            if (isChecked) {
+                Toast.makeText(requireContext(), "Gece modu açık", Toast.LENGTH_SHORT).show()
+            } else Toast.makeText(requireContext(), "Gece modu kapalı", Toast.LENGTH_SHORT).show()
+        }
+
     }
+
 
     private fun loadFragment(fragment: Fragment) {
         val transaction = activity?.supportFragmentManager?.beginTransaction()
